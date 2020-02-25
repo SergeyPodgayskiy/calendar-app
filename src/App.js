dbde6fe3c96381a397753aa601c6ece8cd13edfd
Sergey Podgayskiy <sergeypodgayskiy@gmail.com>
2020-02-25 17:35:51 +0300

    App init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CssBaseline} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import {makeStyles} from "@material-ui/core/styles";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        flexGrow: 1,
+    }
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const classes = useStyles();
+    return (
+      <>
+      <CssBaseline/>
+          <Header/>
+          <Container maxWidth={"lg"} className={classes.container}>
+            hello
+          </Container>
+      </>
   );
 }
 
