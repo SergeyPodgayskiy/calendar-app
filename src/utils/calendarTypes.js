e21729d96b6618bbd944fd2dc6bd153aab002441
Sergey Podgayskiy <sergeypodgayskiy@gmail.com>
2020-03-04 16:12:01 +0300

    CalendarTypes component creation

---
+++ src/utils/calendarTypes.js
@@ -0,0 +1,11 @@
+import { BIRTHDAYS, FAMILY, PERSONAL, REMINDERS, TASKS } from '../constants/constants';
+
+export default {
+  personal: PERSONAL,
+  birthdays: BIRTHDAYS,
+  reminders: REMINDERS,
+  tasks: TASKS,
+  family: FAMILY,
+};
+
+export const DEFAULT_CALENDAR_TYPE = PERSONAL;
